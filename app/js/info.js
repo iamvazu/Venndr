882426eb2af5282e9f32fa9ee316b36a9a6408d3
vardaro <anthonyavardaro@yahoo.com>
2018-01-05 20:46:44 -0600

    add details page

--- app/js/info.js
+++ app/js/info.js
@@ -1,24 +1,43 @@
-$(document).ready(() => {
+const inject = (id, val) => {
+    $(`#${id}`).html(val);
+}
 
-    let url = window.location.search.substring(1);
-    let params = url.split('&');
-    console.log(params);
-
-    // Extract the Id from url, then query db for specific job by id
-    let id = params[0].split('=')[1];
+const queryJob = (id, callback) => {
     $.ajax({
         type: 'GET',
         url: `http://localhost:9000/api/job/${id}`,
     })
-    .done(data => {
-        console.log(data);
+        .done(data => {
+            callback(data);
+        });
+}
+$(document).ready(() => {
+    inject('numResume', JSON.parse(Cookies.get('resume_keywords')).length + " keywords");
+    // get the url and seperate by &
+    let url = window.location.search.substring(1);
+    let params = url.split('&');
 
+    console.log(params);
+
+    // Extract the Id from url, then query db for specific job by id
+    let id = params[0].split('=')[1];
+    queryJob(id, data => {
+        job = data;
+        console.log(job);
+
+        for (let prop in job) {
+                inject(prop, job[prop] || 'N/A');
+        }
+        inject('url', `<a target="_blank" href="${job.url}">Apply</a>`)
+        inject('numKeywords', job.keywords.length + " keywords in desc.")
     });
 
     // parse integer of number of matches form url
     let matches = params[1].split('=')[1];
+    inject('matches', matches + " matching keywords");
 
     // parse array of strings from url, each word in array is split by '%2C'
     let commons = params[2].split('=')[1].split('%2C');
+    inject('commons', commons.join(', '));
 
 });
